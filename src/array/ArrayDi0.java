1deac83f5c92249fcd249e56b1b4355d781d844a
lgsok00 <lgsok00@naver.com>
2024-01-07 00:47:26 +0900

    2차원 배열 - 리팩토링 1

--- src/array/ArrayDi0.java
+++ src/array/ArrayDi0.java
@@ -4,7 +4,7 @@ public class ArrayDi0 {
 
   public static void main(String[] args) {
     // 2 x 3 2차원 배열을 만든다.
-    int [][] arr = new int[2][3];  // 행 = 2, 열 = 3
+    int [][] arr = new int[2][3];  // 행(row) = 2, 열(column) = 3
 
     arr[0][0] = 1;  // 0행 0열
     arr[0][1] = 2;  // 0행 1열
@@ -15,14 +15,14 @@ public class ArrayDi0 {
 
     // 0행 출력
     System.out.print(arr[0][0] + " ");  // 0열 출력
-    System.out.print(arr[0][1] + " ");  // 0열 출력
-    System.out.print(arr[0][2] + " ");  // 0열 출력
+    System.out.print(arr[0][1] + " ");  // 1열 출력
+    System.out.print(arr[0][2] + " ");  // 2열 출력
     System.out.println();  // 한 행이 끝나면 라인을 변경한다.
 
     // 1행 출력
     System.out.print(arr[1][0] + " ");  // 0열 출력
-    System.out.print(arr[1][1] + " ");  //  0열 출력
-    System.out.print(arr[1][2] + " ");  // 0열 출력
+    System.out.print(arr[1][1] + " ");  // 1열 출력
+    System.out.print(arr[1][2] + " ");  // 2열 출력
     System.out.println();  // 한 행이 끝나면 라인을 변경한다.
   }
 }
